1caf45e6562cf66424041e3b4524cfda6a569683
vfedirko <77047995+politttest@users.noreply.github.com>
2023-05-28 22:44:12 +0200

    Add files via upload

---
+++ js/blog.js
@@ -0,0 +1,38 @@
+document.querySelectorAll(".blog-menu-list-point-main-text-container").forEach((e, index) => {
+  let containerParent = e.parentNode;
+  let accordHeight = containerParent.querySelector(".blog-menu-list-point-second-text-container").clientHeight;
+  containerParent.querySelector(".blog-menu-list-point-second-text-container").style.height = "0px";
+  e.addEventListener("click", () => {
+    e.querySelector(".blog-menu-list-point-main-text-image").classList.toggle("active");
+    document.querySelectorAll(".blog-menu-list-point")[index].classList.toggle("active");
+    if (document.querySelectorAll(".blog-menu-list-point")[index].classList.contains("active")) {
+      containerParent.querySelector(".blog-menu-list-point-second-text-container").style.height = accordHeight + "px";
+    } else {
+      containerParent.querySelector(".blog-menu-list-point-second-text-container").style.height = "0px";
+    }
+  });
+});
+
+document.querySelector(".blog-main-mobile-in-button").addEventListener("click", () => {
+  document.querySelector(".blog-main-mobile-content").classList.toggle("active");
+});
+
+document.querySelector(".blog-main-mobile-button").addEventListener("click", () => {
+  document.querySelector(".blog-main-mobile-content").classList.toggle("active");
+});
+
+document.querySelectorAll(".blog-main-mobile-list-point-main-text-container").forEach((e, index) => {
+  let containerParent = e.parentNode;
+  // let accordHeight = containerParent.querySelector(".blog-main-mobile-list-point-second-text-container").clientHeight;
+  // containerParent.querySelector(".blog-main-mobile-list-point-second-text-container").style.height = "0px";
+  e.addEventListener("click", () => {
+    e.querySelector(".blog-main-mobile-list-point-main-text-image").classList.toggle("active");
+    document.querySelectorAll(".blog-main-mobile-list-point")[index].classList.toggle("active");
+    document.querySelectorAll(".blog-main-mobile-list-point-second-text-container")[index].classList.toggle("active");
+    //   if (document.querySelectorAll(".blog-main-mobile-list-point")[index].classList.contains("active")) {
+    //     containerParent.querySelector(".blog-main-mobile-list-point-second-text-container").style.height = accordHeight + "px";
+    //   } else {
+    //     containerParent.querySelector(".blog-main-mobile-list-point-second-text-container").style.height = "0px";
+    //   }
+  });
+});
